b6c3a06860de79d9495c002c5dc28f20889b2434
unknown <natalie9705@gmail.com>
2024-10-14 15:07:12 -0400

    auto commit

--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -3,7 +3,7 @@ struct shaderVars
 {
     float3 position : POSITION;
 };
-float4 main(shaderVars input) : SV_POSITION 
+float4 main(shaderVars input : POSITION) : SV_POSITION 
 {
 	return float4(input.position, 1);
 }
