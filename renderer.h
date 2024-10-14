6bd2a70424289d8318be0e77164c951f1e0a8523
unknown <natalie9705@gmail.com>
2024-10-14 14:13:18 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -673,7 +673,11 @@ public:
 		VkCommandBuffer commandBuffer = GetCurrentCommandBuffer();
 		SetUpPipeline(commandBuffer);
 
-		vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+		//vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+		
+		const tinygltf::Accessor& indexAccessor = model.accessors[model.meshes[0].primitives[0].indices];
+		uint32_t indexCount = indexAccessor.count;
+		vkCmdDrawIndexed(commandBuffer, indexCount, 1, 0, 0, 0);
 	}
 
 private:
