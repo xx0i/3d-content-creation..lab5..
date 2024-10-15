7da90d062f13493915f839bc80d81c91a6543056
unknown <natalie9705@gmail.com>
2024-10-14 21:26:43 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -267,12 +267,6 @@ private:
 			GvkHelper::create_buffer(physicalDevice, device, bufferSize, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
 				VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &storageBufferHandle[i], &storageBufferData[i]);
 			GvkHelper::write_to_buffer(device, storageBufferData[i], indexData, bufferSize);
-			const unsigned short* storageData = reinterpret_cast<const unsigned short*>(&storageBufferData[i]);
-			std::cout << "Storage Buffer " << i << " Indices:\n";
-			for (size_t j = 0; j < bufferSize / sizeof(unsigned short); j++)
-			{
-				std::cout << "Index " << j << ": " << storageData[j] << "\n";
-			}
 		}
 	}
 
